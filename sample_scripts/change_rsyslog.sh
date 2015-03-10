516f84f02e7d475641b9b8f89d134279c907255e
Nilanjan Roy <nilanjan.roy@nosto.com>
2015-03-10 16:40:11 +0200

    Minor improvements

--- sample_scripts/change_rsyslog.sh
+++ sample_scripts/change_rsyslog.sh
@@ -8,13 +8,14 @@ function change_rsyslog() {
 host=$1
 echo "Current server ${host}"
 echo "Uploading 32-graylog2.conf"
-$SCP  32-graylog2.conf ${host}:/tmp
-$SSH $host "sudo mv /tmp/32-graylog2.conf /etc/rsyslog.d/"
+$SCP  32-graylog2.conf ${host}:/tmp || return 1
+$SSH $host "sudo mv /tmp/32-graylog2.conf /etc/rsyslog.d/" || return 1
 echo "Modifying rsyslog.conf"
-$SSH $host "sudo sed -i.sed '/PreserveFQDN on/d' /etc/rsyslog.conf"
+$SSH $host "sudo sed -i.sed '/PreserveFQDN on/d' /etc/rsyslog.conf" || return 1
 echo "Restarting rsyslog"
-$SSH $host "sudo restart rsyslog"
+$SSH $host "sudo restart rsyslog" || return 1
 echo "***********Finished**********"
+return 0
 }
 
 for host in $(cat $hostfile)
