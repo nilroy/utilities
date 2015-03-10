b95e1473c800b0cf941a8609e8289b719e79e4f1
Nilanjan Roy <nilanjan.roy@nosto.com>
2015-03-10 16:33:36 +0200

    Minor improvement

--- sample_scripts/change_rsyslog.sh
+++ sample_scripts/change_rsyslog.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
-set -o verbose
 
-host=$1
+hostfile=$1
 SSH="ssh -o StrictHostKeyChecking=no"
 SCP="scp -o StrictHostKeyChecking=no"
 
+function change_rsyslog() {
+host=$1
+echo "Current server ${host}"
+echo "Uploading 32-graylog2.conf"
 $SCP  32-graylog2.conf ${host}:/tmp
 $SSH $host "sudo mv /tmp/32-graylog2.conf /etc/rsyslog.d/"
+echo "Modifying rsyslog.conf"
 $SSH $host "sudo sed -i.sed '/PreserveFQDN on/d' /etc/rsyslog.conf"
-$SSH $host "sudo restart rsyslog" 
+echo "Restarting rsyslog"
+$SSH $host "sudo restart rsyslog"
+echo "***********Finished**********"
+}
+
+for host in $(cat $hostfile)
+do
+   change_rsyslog $host
+done
